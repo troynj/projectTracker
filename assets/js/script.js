30b75a63d8735eda7ec988c939d89fc53a4292a0
Ian Senne <ianssenne@gmail.com>
2023-01-06 12:27:59 -0800

    create project table

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,25 @@ function readList() {
 	let data = localStorage.getItem("projects");
 	return data === null ? [] : JSON.parse(data);
 }
+
+function updateList() {
+	function makeRow(name, type, date, index) {
+		const row = $("<tr>");
+		row.append($("<td>" + name + "</td>"));
+		row.append($("<td>" + type + "</td>"));
+		row.append($("<td>" + date + "</td>"));
+		let button = $("<button>X</button>");
+		button.attr("data-field", index);
+		row.append($("<td>").append(button));
+		return row;
+	}
+	const data = readList();
+	console.log({ data });
+	let table = $("#table-content");
+	table.empty();
+	data.forEach((project, index) => {
+		table.append(makeRow(project.name, project.type, project.dueDate, index));
+	});
+}
+$("#table").on("click", "*[data-field]", (e) => {});
+updateList();
